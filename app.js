608dc594987e0e13b5e5562fa81190c768ab1950
TuncayArican <tuncayarican07@gmail.com>
2022-03-05 11:55:07 +0300

    Added a function for clean code

--- app.js
+++ app.js
@@ -13,14 +13,12 @@ window.addEventListener('load', () => {
         
 
         // I have added "0" if the Hours/minutes/Seconds smaller than 10
-        hours=hours<10 ? "0" + hours : hours
-        showHour.innerText = hours 
-        minutes=minutes<10 ? "0" + minutes : minutes
-        showMinute.innerText = minutes
-        seconds=seconds<10 ? "0" + seconds : seconds
-        showSecond.innerText = seconds
-        
+        let singleDigit= no =>no<10 ? "0" + no : no;
 
+        showHour.innerText = singleDigit(hours)
+        showMinute.innerText = singleDigit(minutes)
+        showSecond.innerText = singleDigit(seconds)
+        
         //We will add AM or PM according to the hour
         ampm.innerText= hours>=12 ? "PM" : "AM"
     }, 1000)
